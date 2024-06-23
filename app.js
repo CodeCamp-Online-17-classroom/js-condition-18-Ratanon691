607be7f6e72a03831626955a742a3940265d52bd
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-24 01:44:48 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-// write code here
+let login = prompt('Enter username');
+let message = login === `Employee` ? `Hello`: 
+              login === `Director` ? `Greeting`: 
+              login === `` ? `No login` : ``                        
+console.log(message)
